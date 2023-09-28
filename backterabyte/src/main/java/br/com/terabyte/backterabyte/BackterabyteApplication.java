a8b7cfa4c44d45321f114981fc3c0f7e8020fb6c
Lucas gabriel neves da silva santos <dexdex2000@hotmail.com>
2023-09-28 14:01:00 -0300

    update

---
+++ backterabyte/src/main/java/br/com/terabyte/backterabyte/BackterabyteApplication.java
@@ -0,0 +1,13 @@
+package br.com.terabyte.backterabyte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackterabyteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackterabyteApplication.class, args);
+	}
+
+}
---
+++ backterabyte/src/test/java/br/com/terabyte/backterabyte/BackterabyteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.terabyte.backterabyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackterabyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
