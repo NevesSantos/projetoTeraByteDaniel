9374d5351b0c2bfa4ca1513406ffe94be2923adc
Lucas gabriel neves da silva santos <dexdex2000@hotmail.com>
2023-10-02 16:50:20 -0300

    update

--- front/js tabela cadastrar/funcoesglobais.js
+++ front/js tabela cadastrar/funcoesglobais.js
@@ -15,10 +15,7 @@ function limpaTudo(){
 
 
 function fica_em_alerta(){
-    if(inputNome.value.trim() === ""){
-        alert("<3 O campo Nome deve ser preenchido besta :D <3");
-        return true;
-    }else if(inputImg.value.trim() === "" || inputDesc.value.trim() === "" || inputPreco.value.trim() === ""){
+    if(inputImg.value.trim() === "" || inputDesc.value.trim() === "" || inputPreco.value.trim() === ""){
         alert("<3 Preenche tudo mano aff :D <3");
         return true;
     }
--- front/js tabela cadastrar/post.js
+++ front/js tabela cadastrar/post.js
@@ -1,13 +1,14 @@
 document.querySelector('form').addEventListener('submit', function(e){
     e.preventDefault();
-    fica_em_alerta();
     
-    const dadosFormulario = new FormData(this);
-    fetch("http://localhost:8080/", {
-        method: "POST",
-        body: dadosFormulario
-    })
-    .then(resposta => resposta.json())
-    .then(produto => inserirProduto(produto))
-    .catch(erro => console.log("Ocorreu um erro ao cadastrar produto: " + erro));
+    if (!fica_em_alerta()){
+        const dadosFormulario = new FormData(this);
+        fetch("http://localhost:8080/", {
+            method: "POST",
+            body: dadosFormulario
+        })
+        .then(resposta => resposta.json())
+        .then(produto => inserirProduto(produto))
+        .catch(erro => console.log("Ocorreu um erro ao cadastrar produto: " + erro));
+    };
 });
